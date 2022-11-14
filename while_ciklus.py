80d14a69c8f685c806a369a09949537d1b6e8933
kandoikt <75670096+kandoikt@users.noreply.github.com>
2022-11-14 09:43:05 +0100

    Add files via upload

---
+++ while_ciklus.py
@@ -0,0 +1,48 @@
+"""
+szam = 0
+while szam != 100:
+    szam = szam + 1
+    print(szam)
+print("A program vége!")
+# 1..100
+
+
+szam = 0
+while szam != 100:
+    print(szam)
+    szam = szam + 1
+print("A program vége!")
+# 0..99
+
+
+# dk 3. feladat
+szam = 1
+while szam != 101:
+    print(szam)
+    szam = szam + 1
+print("A program vége!")
+# 1..100
+
+
+
+
+szam = 1
+while True:
+    print(szam)
+    szam = szam + 1
+print("A program vége!")
+# a program futását "CTRL"+"C" -vel tudjuk leállítani
+
+
+# dk 2. feladat
+
+szam = 0
+while szam < 100:
+    szam = szam + 1
+    print(szam)
+print("A program vége!")
+# 1..100
+
+"""
+
+
